728261bb13d133c11798fea9d8a28aea58b53828
ShiXianjun <1092207960@qq.com>
2019-05-28 11:02:17 +0800

    基础代码

---
+++ UICollectionViewTetris/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewTetris
+//
+//  Created by 石显军 on 2019/5/28.
+//  Copyright © 2019 石显军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewTetris/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewTetris
+//
+//  Created by 石显军 on 2019/5/28.
+//  Copyright © 2019 石显军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
